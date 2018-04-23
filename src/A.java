5cc0fb7d8871fa2de7810020a170db95318acee7
Sviatlana Zakharenka <sviatlana_zakharenka@epam.com>
2018-04-23 20:40:05 +0300

    New hello message

--- src/A.java
+++ src/A.java
@@ -1,8 +1,7 @@
 public class A{
 	
 	public static void main(String[] args){
-		System.out.println("Hello from SZ");
-		System.out.println("Hello from SZ2");
+		System.out.println("Hello Dear Students!");
 	}
 	
 }
