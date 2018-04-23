e847ea198ce08598fc854c7aa802e512b71d9d40
Sviatlana Zakharenka <sviatlana_zakharenka@epam.com>
2018-04-23 20:56:00 +0300

    new local changes

--- src/A.java
+++ src/A.java
@@ -3,7 +3,7 @@ public class A{
 	public static void main(String[] args){
 		System.out.println("Hello Dear Students!");
 		System.out.println("Hello Dear Students2!");
-		System.out.println("Hello Dear Students2!");
+		System.out.println("Hello Dear Students3!");
 	}
 	
 }
