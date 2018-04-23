f5f552410f6d1e9db05a2426168b43baf5871fe4
Sviatlana Zakharenka <sviatlana_zakharenka@epam.com>
2018-04-23 21:18:17 +0300

    conflict result2

--- src/A.java
+++ src/A.java
@@ -4,11 +4,9 @@ public class A{
 		System.out.println("Hello Dear Students!");
 		System.out.println("Hello Dear Students2!");
 		System.out.println("Hello Dear Students3!");
-<<<<<<< HEAD
+
 		System.out.println("Hello Dear Students4!");
-=======
 		System.out.println("Hello from web");
->>>>>>> 00e7f31aae0db17bcc58bda7f81fdc4830584c3b
 	}
 	
 }
