00e7f31aae0db17bcc58bda7f81fdc4830584c3b
HTPTAT7 <36242179+HTPTAT7@users.noreply.github.com>
2018-04-23 20:59:41 +0300

    web changes

--- src/A.java
+++ src/A.java
@@ -4,6 +4,7 @@ public class A{
 		System.out.println("Hello Dear Students!");
 		System.out.println("Hello Dear Students2!");
 		System.out.println("Hello Dear Students3!");
+		System.out.println("Hello from web");
 	}
 	
 }
