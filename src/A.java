36c1fc46ff14a6cadfa087fd6e98e80a1665a1e9
Nikolay Tsutsalevich <36222869+Cantehnick@users.noreply.github.com>
2018-04-23 22:03:38 +0300

    new commit by Nick

--- src/A.java
+++ src/A.java
@@ -10,7 +10,7 @@ public class A{
 
 		System.out.println("Hello Dear Students4!");
 		System.out.println("Hello from web");
-	System.out.println("Git push from Nikolay");
+	System.out.println("Git push from Nikolay!!!");
 
 }
 
